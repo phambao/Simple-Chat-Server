659449251898842480ff49f25404f37724b0e52b
phambao <baopq97@gmail.com>
2022-04-05 13:46:00 +0700

    updated

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,7 +2,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from chat.models import Room
+from .models import Room
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
