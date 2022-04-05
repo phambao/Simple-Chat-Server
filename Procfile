346c19f82a9ba9eec8662bd575a8d955dcc79c85
phambao <baopq97@gmail.com>
2022-04-05 15:46:38 +0700

    updated

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: daphne django_channel.asgi:channel_layer  --port $PORT --bind 0.0.0.0 -v2
+web: daphne django_channel.asgi:application  --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker channels --settings=django_channel.settings -v2
--- django_channel/asgi.py
+++ django_channel/asgi.py
@@ -1,17 +1,18 @@
 # mysite/asgi.py
 import os
 import django
+from django.core.asgi import get_asgi_application
+django.setup()
+django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import chat.routing
-django.setup()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_channel.settings")
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
