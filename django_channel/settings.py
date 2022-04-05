fa20361d554721519f155bf2d08fca3bd55dcd40
phambao <baopq97@gmail.com>
2022-04-05 12:14:40 +0700

    update DEBUG for deploy

--- django_channel/settings.py
+++ django_channel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h*imak+khvr)nlu8k_6w)=v-ep98w&tffs)=54pt)y4ht84a%4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
